c6a4a02b9ee0e6ed8c7ad1e4a790625e36683c12
Daniel Northcutt <daniel.h.northcutt@gmail.com>
2022-01-31 11:05:25 -0600

    updated

--- prepare.py
+++ prepare.py
@@ -29,6 +29,9 @@ def clean_cohort(df):
     # creating subpath for page hits
     df['subpath'] = df.path.apply(lambda x: x.split('/'))
 
+    #creates counter column for ip address question
+    df['counter'] = 1
+
     return df
 
 def program_split(df):
@@ -40,9 +43,57 @@ def program_split(df):
     # # exploraiton in scratchpad - leaves 29 values for ds of course material viewed
     # df_ds['url'] = df_ds['url'].value_counts().loc[lambda x : x>100] 
     # # note: some values need to be grouped
-
-
     # # same as above for web dev - leaves 23 values
     # df_wd['url'] = df_wd['url'].value_counts().loc[lambda x : x>100]
 
+    #creates lesson column for df_ds and labels materials
+    df_ds['lesson'] = np.where(df_ds.path.str.contains('appendix'), 'appendix',
+    np.where(df_ds.path.str.contains('search'), 'search',
+    np.where(df_ds.path.str.contains('classification'),'classification',
+    np.where(df_ds.path.str.contains('sql'), 'sql',
+    np.where(df_ds.path.str.contains('fundamentals'), 'fundamentals',
+    np.where(df_ds.path.str.contains('regression'), 'regression',
+    np.where(df_ds.path.str.contains('python'), 'python',
+    np.where(df_ds.path.str.contains('stats'), 'stats', 
+    np.where(df_ds.path.str.contains('anomaly'), 'anomaly',
+    np.where(df_ds.path.str.contains('clustering'), 'clustering',
+    np.where(df_ds.path.str.contains('nlp'), 'nlp',
+    np.where(df_ds.path.str.contains('timeseries'), 'time_series',
+    np.where(df_ds.path.str.contains('distributed-ml'), 'distributed_ml''',
+    np.where(df_ds.path.str.contains('storytelling'), 'storytelling',
+
+    np.where(df_ds.path.str.contains('advanced-topics'), 'advanced-topics',
+    np.where(df_ds.path.str.contains('capstones'), 'capstones',
+    'pending'))))))))))))))))
+    df_ds.lesson.unique()
+
+    # created lesson column with values for web dev lessons
+    df_wd['lesson'] = np.where(df_wd.path.str.contains('search'),'search',
+    np.where(df_wd.path.str.contains('index'),'index',
+    np.where(df_wd.path.str.contains('javascript'),'javascript',
+    np.where(df_wd.path.str.contains('toc'),'toc',
+    np.where(df_wd.path.str.contains('java'),'java',
+    np.where(df_wd.path.str.contains('html|css'),'html-css',
+    np.where(df_wd.path.str.contains('spring'),'spring',
+    np.where(df_wd.path.str.contains('jquery'),'jquery',
+    np.where(df_wd.path.str.contains('mysql'),'mysql',
+    np.where(df_wd.path.str.contains('capstone'),'capstone',
+    np.where(df_wd.path.str.contains('array|syntax|object_oriented|polymorph|methods|collections|deployment'),'structure',
+    np.where(df_wd.path.str.contains('php'),'php',
+    np.where(df_wd.path.str.contains('larvel'),'larvel',
+            
+    'pending')))))))))))))
+                                                                                                                                                
+    df_wd.lesson.unique()
+
+
+    # # set datetime & index
+    # df_ds.date = pd.to_datetime(df_ds.date)
+    # df_ds =df_ds.set_index('date')
+    # # set datetime & index
+    # df_wd.date = pd.to_datetime(df_wd.date)
+    # df_wd =df_wd.set_index('date')
+
+
+    #returns the two dfs
     return df_wd, df_ds
